3667207088c245af4c6d8190be829a19843e910c
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-05 20:25:58 -0400

    Add slides for intro to RxJS (#1)
    This commit adds the slides for the introduction to RxJS.

--- presentation/index.js
+++ presentation/index.js
@@ -4,10 +4,10 @@ import React, { Component } from 'react'
 
 // Import Spectacle Core tags
 import {
-  Appear,
-  // BlockQuote,
-  // Cite,
-  //  CodePane,
+  // Appear,
+  BlockQuote,
+  Cite,
+  // CodePane,
   Deck,
   //  Fill,
   Heading,
@@ -16,15 +16,15 @@ import {
   //  Link,
   ListItem,
   List,
-  //  Markdown,
-  // Quote,
+  Markdown,
+  Quote,
   Slide,
   Spectacle,
-  // Text,
+  Text,
 } from 'spectacle'
 
 // Import code presentation component
-// import CodeSlide from 'spectacle-code-slide'
+import CodeSlide from 'spectacle-code-slide'
 
 // Import image preloader util
 // import preloader from 'spectacle/lib/utils/preloader'
@@ -77,6 +77,23 @@ const styles = {
   },
 }
 
+const references = `
+  ## My Reading Materials
+
+  -  [**Learning Observable By Building Observable**](https://medium.com/@benlesh/learning-observable-by-building-observable-d5da57405d87#.nme5giluq)
+  -  [**Hot vs Cold Observables**](https://medium.com/@benlesh/hot-vs-cold-observables-f8094ed53339#.7aen7c7nm)
+  -  [**RxJS 5**](https://github.com/ReactiveX/rxjs)
+  -  [**Rx Marbles**](http://rxmarbles.com/)
+`
+
+const follow = `
+  ## Follow these amazing people on Twitter!
+
+  - [@BenLesh](https://twitter.com/benlesh?lang=en)
+  - [@AndreStaltz](https://twitter.com/andrestaltz)
+  - [@MattPodwysocki](https://twitter.com/mattpodwysocki)
+`
+
 class Presentation extends Component {
   render () {
     return (
@@ -119,6 +136,117 @@ class Presentation extends Component {
                 GitHub: <a target='_blank' href={urls.adamGithub}>@adamrecvlohe</a>
               </ListItem>
             </List>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Heading size={1} textColor='primary'>History</Heading>
+            <Heading textColor='primary'>&&</Heading>
+            <Heading size={1} textColor='primary'>Background</Heading>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Text textColor='primary'>Some Info on the Not So Obvious</Text>
+            <List>
+              <ListItem textColor='primary'>RxJS stands for Reactive Extensions for JavaScript</ListItem>
+              <ListItem textColor='primary'>Inspired by linq.js ... basically .NET in JS</ListItem>
+              <ListItem textColor='primary'>Erik Meijer (@headinthebox) created Rx</ListItem>
+              <ListItem textColor='primary'>Matthew Podwysocki (@mattpodwysocki) ported Rx to RxJS</ListItem>
+            </List>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <BlockQuote>
+              <Quote textColor='primary'>
+                When you iterate over an array you are pulling values out. Reactive programming is about pushing/producing those values.
+              </Quote>
+            </BlockQuote>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>What creates these values and how are they pushed?</Heading>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>The Observable</Heading>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <BlockQuote>
+              <Quote>Observable is just a function that takes an observer and returns a function.</Quote>
+              <Cite textColor='primary'>Ben Lesh</Cite>
+            </BlockQuote>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>Coding Time!</Heading>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang='js'
+            code={require('raw!../assets/code_slides/observable.example')}
+            ranges={[
+              { loc: [0, 29], title: `Let's create an observable` },
+              { loc: [0, 5], note: 'Create a function that takes an observer' },
+              { loc: [6, 11], note: 'Create an observer object that has next, error, and complete functions' },
+              { loc: [12, 18], note: `Let's now produce some values` },
+              { loc: [19, 24], note: 'And we can tell our observer how we will compute/transform those values' },
+              { loc: [25, 29], note: 'If we call our function...' },
+            ]} />
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>Observables and Operators</Heading>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Text caps textColor='primary'>Observable</Text>
+            <List>
+              <ListItem textColor='primary'>fromEvent(element, event) => Observable</ListItem>
+            </List>
+            <Text caps textColor='primary'>Operators</Text>
+            <List textColor='primary'>
+              <ListItem>mapTo(Observable) => Observable</ListItem>
+              <ListItem>merge(Observable1, Observable2, etc.) => Observable</ListItem>
+              <ListItem>scan(accumulator, Observable) => Observable </ListItem>
+            </List>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>Coding Time!</Heading>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang='js'
+            code={require('raw!../assets/code_slides/counter.example')}
+            ranges={[
+              { loc: [0, 41], title: `A counter in RxJS` },
+              { loc: [3, 15], note: 'Boilerplate' },
+              { loc: [16, 18] },
+              { loc: [19, 22] },
+              { loc: [23, 26] },
+              { loc: [27, 29] },
+              { loc: [30, 32] },
+              { loc: [33, 39] },
+            ]} />
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>Hot vs Cold</Heading>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Quote>
+              When your observable creates the producer, your observable is cold. When your producer is created outside of a subscription your observable is hot.
+            </Quote>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>Coding Time!</Heading>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang='js'
+            code={require('raw!../assets/code_slides/bgcolor.example')}
+            ranges={[
+              { loc: [0, 26], title: `Change bgColor in RxJS` },
+              { loc: [3, 9], note: 'Boilerplate' },
+              { loc: [10, 12] },
+              { loc: [13, 17] },
+              { loc: [18, 26] },
+            ]} />
+          <Slide bgColor='tertiary'>
+            <Heading textColor='primary'>References</Heading>
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Markdown source={references} />
+          </Slide>
+          <Slide bgColor='tertiary'>
+            <Markdown source={follow} />
           </Slide>
           <Slide bgColor='primary'>
             <Heading size={1} caps fit textColor='secondary' textFont='primary'>
